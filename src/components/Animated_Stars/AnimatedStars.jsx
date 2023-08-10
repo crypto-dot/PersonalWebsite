4c2eeb2fbe02686f8f830c8a9c39e2bba5968cae
Carlos <carlos2345532@gmail.com>
2023-08-10 18:13:46 -0500

    heading updates, color updates, font updates, sass now being used, new folder for animated stars

---
+++ src/components/Animated_Stars/AnimatedStars.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function AnimatedStars() {
+  return (
+    <div>AnimatedStars</div>
+  )
+}
+
+export default AnimatedStars;
--- src/components/Introduction/Introduction.jsx
+++ src/components/Introduction/Introduction.jsx
@@ -1,34 +1,41 @@
 import { useEffect, useRef } from "preact/hooks";
 import person from "../../assets/person.png";
-import "./Introduction.css";
+import "./Introduction.scss";
 const Introduction = () => {
+    const svg = useRef();
+    const button = useRef();
     const handleResize = () => {
 
     };
-    const handleClick = (e) => {
-        console.log(e.target);
+    const handleClick = () => {
+        svg.current.classList.add("animate");
+
     };  
-    const buttonRef = useRef();
     useEffect(()=> {
         window.addEventListener("resize", handleResize);
     },[]);
     return (    
         <>
-            <nav>
-                <button onClick={handleClick} ref={buttonRef} aria-hidden="false" aria-label="open the mobile nav menu">
-
+            <nav className="nav">
+                <h2 className="h2__name">Carlos Arbizu</h2>
+                <button className="button__mobile" ref={button} onClick={handleClick} aria-hidden="false" aria-label="open the mobile nav menu">
+                <svg ref={svg} xmlns="http://www.w3.org/2000/svg" width="26" height="16" viewBox="0 0 26 16" fill="none">
+                    <path d="M2 2H24.4" stroke="black" stroke-width="3" stroke-linecap="round"/>
+                    <path d="M24.4 14.4H2" stroke="black" stroke-width="3" stroke-linecap="round"/>
+                </svg>
                 </button>
-                <ul>
-                    <li><a href="">About</a></li>
-                    <li><a href="">Projects</a></li>
-                    <li><a href="">Posts</a></li>
+                <ul className="ul__nav" style={"display: none"}>
+                    <li className="li__nav"><a className="a__nav" href="">About</a></li>
+                    <li className="li__nav"><a className="a__nav" href="">Projects</a></li>
+                    <li className="li__nav"><a className="a__nav" href="">Posts</a></li>
                 </ul>
             </nav>
-            <heading>
-                <h1>Portfolio</h1>
-            </heading>
+            <header className="heading__Main">
+                <h1 className="h1">Portfolio</h1>
+                <h2 className="h2__animate">Frontend Developer</h2>
+            </header>
         </>
     );
 }
 
-export default Introduction
+export default Introduction;
