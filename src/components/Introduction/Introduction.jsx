195dd293505cdb469fa09a20c8a66bd3cf3014c1
Carlos <carlos2345532@gmail.com>
2023-08-13 16:30:47 -0500

    semantic html fix, dialog positioning

--- src/components/Introduction/Introduction.jsx
+++ src/components/Introduction/Introduction.jsx
@@ -8,22 +8,30 @@ const Introduction = () => {
     const lineTop = useRef();
     const lineBottom = useRef();
     const dialog = useRef();
+    const h1 = useRef();
     const [isActive, setIsActive] = useState(false);
     const handleResize = () => {
-
+        dialog.current.style.paddingLeft = `${h1.current.getBoundingClientRect().left}px`;
+        buttonClone.current.style.left = `${button.current.getBoundingClientRect().left}px`;
     };
+    const addActiveClasses = () => {
+        lineTop.current.classList.add("line__top--active");
+        lineBottom.current.classList.add("line__bottom--active");
+        lineBottom.current.classList.remove("line__bottom--deactive");
+        lineTop.current.classList.remove("line__top--deactive");
+    }
+    const addDeactiveClasses = () => {
+        lineTop.current.classList.remove("line__top--active");
+        lineBottom.current.classList.remove("line__bottom--active");
+        lineTop.current.classList.add("line__top--deactive");
+        lineBottom.current.classList.add("line__bottom--deactive");
+    }
     const handleClick = () => {
         if(!isActive) {
-            lineTop.current.classList.add("line__top--active");
-            lineBottom.current.classList.add("line__bottom--active");
-            lineBottom.current.classList.remove("line__bottom--deactive");
-            lineTop.current.classList.remove("line__top--deactive");
+            addActiveClasses();
             dialog.current.showModal();
         } else {
-            lineTop.current.classList.remove("line__top--active");
-            lineBottom.current.classList.remove("line__bottom--active");
-            lineTop.current.classList.add("line__top--deactive");
-            lineBottom.current.classList.add("line__bottom--deactive");
+            addDeactiveClasses();
         }
         setIsActive(!isActive);
     };  
@@ -33,11 +41,19 @@ const Introduction = () => {
     }
     useEffect(()=> {
         window.addEventListener("resize", handleResize);
+        dialog.current.style.paddingLeft = `${h1.current.getBoundingClientRect().left}px`;
+        buttonClone.current.style.left = `${button.current.getBoundingClientRect().left}px`;
     },[]);
     return (    
-        <>
+        <header className="heading__Main">
             <nav className="nav">
-                <h2 className="h2__name">Carlos Arbizu</h2>
+                <h1 className="h1__name"  ref={h1}>Carlos Arbizu</h1>
+                <button className="button__mobile" ref={button} onClick={handleClick} aria-hidden={isActive} aria-label="open the mobile nav menu">
+                <svg ref={svg} xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 26 16" fill="none">
+                    <path ref={lineTop} className="line" d="M2 2H24.4" stroke="black" stroke-width="3" stroke-linecap="round"/>
+                    <path ref={lineBottom} className="line" d="M24.4 14.4H2" stroke="black" stroke-width="3" stroke-linecap="round"/>
+                </svg>
+                </button>
                 <dialog className="dialog__main" ref={dialog}>                
                     <ul className="ul__nav">
                     <li className="li__nav"><a className="a__nav" href="">About</a></li>
@@ -46,18 +62,10 @@ const Introduction = () => {
                 </ul>
                 <button ref={buttonClone} onClick={handleClickClone} aria-hidden="true" aria-label="open the mobile nav menu" class="button__mobileClone"><svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 26 16" fill="none"><path d="M2 2H24.4" stroke="black" stroke-width="3" stroke-linecap="round" class="line line__top--active"></path><path d="M24.4 14.4H2" stroke="black" stroke-width="3" stroke-linecap="round" class="line line__bottom--active"></path></svg></button>
                 </dialog>
-                <button className="button__mobile" ref={button} onClick={handleClick} aria-hidden={isActive} aria-label="open the mobile nav menu">
-                <svg ref={svg} xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 26 16" fill="none">
-                    <path ref={lineTop} className="line" d="M2 2H24.4" stroke="black" stroke-width="3" stroke-linecap="round"/>
-                    <path ref={lineBottom} className="line" d="M24.4 14.4H2" stroke="black" stroke-width="3" stroke-linecap="round"/>
-                </svg>
-                </button>
             </nav>
-            <header className="heading__Main">
-                <h1 className="h1">Portfolio</h1>
-                <h2 className="h2__animate">Frontend Developer</h2>
-            </header>
-        </>
+                <h2 className="h2__large">Portfolio</h2>
+                <h3 className="h3__animate">Frontend Developer</h3>
+        </header>
     );
 }
 
